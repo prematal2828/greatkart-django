82f4065a4abfdd1c9e27dba6ccaa884914bc3ee4
prematal2828 <prematal2828@gmail.com>
2023-03-11 04:14:37 +0600

    Added Reset Password

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -141,7 +141,7 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'prematal2828@gmail.com'
-EMAIL_HOST_PASSWORD = 'xeiqhkwiegdxjzrm'
+EMAIL_HOST_PASSWORD = 'asefasdf'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
